4de25ff651024e13151691c69d203e9f395bb050
Anurag M <104742471+code-rastha@users.noreply.github.com>
2022-05-02 15:49:06 -0400

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My New custom Healthy Diner")
+
+streamlit.header("Boba Juice")
+streamlit.text("Taro")
+streamlit.header("Vanilla")
+streamlit.header("all mix")
